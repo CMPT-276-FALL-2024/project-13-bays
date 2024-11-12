c436e7698795451802fe6d814f5c9e6f31a36e6d
Moc01 <lwa124@sfu.ca>
2024-11-12 13:44:23 -0700

    added the HorizontalScrollable component

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,17 +1,17 @@
 import "../styles/components/Forecast.scss"
 import HourlyForecastWidegt from "./HourlyForecastWidegt";
 import DailyForecastWidegt from "./DailyForecastWidegt";
-
+import HorizontallyScrollable from "./HorizontallyScrollable";
 function Forecast({ title, type, data }) {
     console.log('Forecast data:', data);
     return (
         <div className="Forecast">
             <div className="forecast-container">
                 <h3>{title}</h3>
-                <div className="widget-container">
+                <HorizontallyScrollable className="widget-container">
                     {
-                        data?.map((singleData, index) => (
-                            <div key={index}>
+                        data.map((singleData) => (
+                            <div key={singleData.date || singleData.day}>
                                 {type === "hourly" ? 
                                     (<HourlyForecastWidegt data={singleData}/>) : 
                                     (<DailyForecastWidegt data={singleData}/>)
@@ -19,7 +19,7 @@ function Forecast({ title, type, data }) {
                             </div>
                         ))
                     }
-                </div>
+                </HorizontallyScrollable>
             </div>
                     
         </div> );
