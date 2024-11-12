c3633fd7183732bb39e003f66bf2340346c22b87
Moc01 <lwa124@sfu.ca>
2024-11-11 18:24:46 -0700

    added the current weather component & weather icons

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,12 @@
+import CurrentWeather from './CurrentWeather.js'
 import '../styles/components/Main.scss'
+
 function Main(){
-    return <div className='Main'>Main</div>;
+    return (
+        <div className='Main'>
+            <CurrentWeather/>
+        </div>      
+    );
 }
 
 export default Main;
