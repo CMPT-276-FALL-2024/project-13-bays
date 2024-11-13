c74187212888737838a8c6fcd056e20538d47e7c
Moc01 <lwa124@sfu.ca>
2024-11-12 17:00:33 -0700

    added the theme context

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,14 +1,16 @@
 import '../styles/components/Settings.scss'
+import ThemeContext from '../context/theme.context';
 
 function Settings(){
+    const { dark, setDark } = useContext(ThemeContext);
     return (
         <div className="Settings">
             <div className="theme-toggler">
-                <div className="theme-buttons">
-                    <div className="light-theme-btn">
+                <div className="theme-buttons" onClick={() => setDark(!dark)}>
+                    <div className={`light-theme-btn ${dark? '':'active'}`}>
                         <i className="bi bi-sun"></i>
                     </div>
-                    <div className="dark-theme-btn active">
+                    <div className={`dark-theme-btn ${dark? 'active':''}`}>
                     <i className="bi bi-moon"></i>
                     </div>
                 </div>
