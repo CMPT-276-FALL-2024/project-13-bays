0be1a5f54f69de26b6bcb15662412d56ec319da2
odesa <northlycoris@gmail.com>
2024-12-03 21:12:29 -0800

    feat: add about us and fix gitignore

--- src/public/js/fetchWeather.js
+++
@@ -1,9 +0,0 @@
-fetch(
-    'https://api.brightsky.dev/radar'
-).then((resp) => resp.json()
-).then((respData) => {
-    const raw = respData.radar[0].precipitation_5;
-    const compressed = Uint8Array.from(atob(raw), c => c.charCodeAt(0));
-    const rawBytes = pako.inflate(compressed).buffer;
-    const precipitation = new Uint16Array(rawBytes);
-});
